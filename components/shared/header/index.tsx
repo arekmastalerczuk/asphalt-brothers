ab09d8813eb4c3b3d9bfea0a1caaa2090bf1e490
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-09-23 13:16:33 +0200

    add sheet ui component, moved right menu into separate Menu component with sheet menu on sm screens

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { UserIcon, ShoppingCart } from "lucide-react";
 import { APP_NAME } from "@/lib/constants";
-import { Button } from "@/components/ui/button";
-import ModeToggle from "./ModeToggle";
+import Menu from "./Menu";
 
 const Header = () => {
   return (
@@ -24,19 +22,7 @@ const Header = () => {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
