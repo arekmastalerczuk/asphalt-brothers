a2095e47f6afdbd86055e75575958bcdffba7111
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-09-23 11:39:53 +0200

    add loading page and spinner

---
+++ app/loading.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Image from "next/image";
+import Header from "@/components/shared/header";
+import loader from "@/assets/loader.gif";
+import Footer from "@/components/Footer";
+
+const Loading = () => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <div className="flex-center flex-1">
+        <Image src={loader} height={150} width={150} alt="Loading spinner" />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Loading;
