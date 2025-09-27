1a884e5b4e9810713d643961a0448c290ffd0f43
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-09-27 14:40:41 +0200

    add few dependencies into project like: neon, web sockets etc., add configuration into schema.prisma, add db/prisma.ts configuration file, update prisma import in actions

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,13 +1,11 @@
 "use server";
 
-import { PrismaClient } from "../generated/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { prisma } from "@/db/prisma";
 
 // Get latest products
 export const getLatestProducts = async () => {
-  const prisma = new PrismaClient();
-
   const products = await prisma.product.findMany({
     orderBy: { createdAt: "desc" },
     take: LATEST_PRODUCTS_LIMIT,
