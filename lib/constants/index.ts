b3feb0138442bde45f10f83d5527b3774b37c5d0
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-10-06 12:31:31 +0200

    add example sign in and sign up constant values (cleared)

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -17,3 +17,9 @@ export const signInDefaultValues = {
   email: "",
   password: "",
 };
+export const signUpDefaultValues = {
+  name: "",
+  email: "",
+  password: "",
+  passwordConfirm: "",
+};
