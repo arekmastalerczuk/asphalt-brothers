2d87ecff50aeaa0a8d19378b4abb6e8c24e782ce
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-10-08 12:56:15 +0200

    add sonner toast to the project, set default close button in Sonner popup,add Toaster component to the layout, change default position and duration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 import "@/assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
@@ -34,6 +35,7 @@ export default function RootLayout({ children }: Props) {
           disableTransitionOnChange
         >
           {children}
+          <Toaster position="top-center" duration={5000} />
         </ThemeProvider>
       </body>
     </html>
