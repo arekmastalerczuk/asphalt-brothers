383b1eade3e6520ecd3073b129294a9441537d59
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-11-11 18:05:42 +0100

    refactor handleResponse, add paypal methods: createOrder and capturePayment

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,7 +1,48 @@
+import { createOrder } from "@/lib/actions/order.actions";
 const base_url =
   process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+    const url = `${base_url}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+
+    return handleResponse(response);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base_url}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return handleResponse(response);
+  },
+};
 
 // Generate paypal access token
 async function generateAccessToken() {
@@ -20,14 +61,17 @@ async function generateAccessToken() {
     body: "grant_type=client_credentials",
   });
 
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
+
+async function handleResponse(response: Response) {
   if (!response.ok) {
     const errorMessage = await response.text();
     throw new Error(errorMessage);
   }
 
-  const jsonData = await response.json();
-
-  return jsonData.access_token;
+  return response.json();
 }
 
 export { generateAccessToken };
