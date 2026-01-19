69d3b1798837a85f6e3c038caee8b64f01e0f51c
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2026-01-19 19:27:04 +0100

    added admin Dashboard page without Overview section

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,4 +1,130 @@
-const page = () => {
-  return <div>Overview</div>;
+import { auth } from "@/auth";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { getOrderSummary } from "@/lib/actions/order.actions";
+import { formatCurrency, formatDateTime, formatNumber } from "@/lib/utils";
+import { BadgeDollarSign, Barcode, CreditCard, Eye, Users } from "lucide-react";
+import { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
 };
-export default page;
+
+const AdminOverviewPage = async () => {
+  const session = await auth();
+
+  if (session?.user?.role !== "admin")
+    throw new Error("User is not authorized");
+
+  const summary = await getOrderSummary();
+
+  return (
+    <div className="space-y-2">
+      <h1 className="h2-bold">Dashboard</h1>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+            <BadgeDollarSign />
+          </CardHeader>
+          <CardContent>
+            <p className="text-2xl font-bold">
+              {formatCurrency(
+                summary?.totalSales?._sum?.totalPrice?.toString() || "0",
+              )}
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Sales</CardTitle>
+            <CreditCard />
+          </CardHeader>
+          <CardContent>
+            <p className="text-2xl font-bold">
+              {formatNumber(summary?.ordersCount) || 0}
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Customers</CardTitle>
+            <Users />
+          </CardHeader>
+          <CardContent>
+            <p className="text-2xl font-bold">
+              {formatNumber(summary?.usersCount) || 0}
+            </p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Products</CardTitle>
+            <Barcode />
+          </CardHeader>
+          <CardContent>
+            <p className="text-2xl font-bold">
+              {formatNumber(summary?.productsCount) || 0}
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+        <Card className="lg:col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent>{/* TODO CHART */}</CardContent>
+        </Card>
+        <Card className="lg:col-span-3">
+          <CardHeader>
+            <CardTitle>Recent Sales</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Buyer</TableHead>
+                  <TableHead>Date</TableHead>
+                  <TableHead>Total</TableHead>
+                  <TableHead>Actions</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {summary?.latestSales?.map((order) => (
+                  <TableRow key={order.id}>
+                    <TableCell>{order?.user?.name || "Deleted user"}</TableCell>
+                    <TableCell>
+                      {formatDateTime(order?.createdAt).dateOnly}
+                    </TableCell>
+                    <TableCell>
+                      {formatCurrency(Number(order?.totalPrice))}
+                    </TableCell>
+                    <TableCell>
+                      <Link
+                        href={`/order/${order.id}`}
+                        className="flex items-center space-x-2"
+                      >
+                        <span>View</span>
+                        <Eye />
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+export default AdminOverviewPage;
